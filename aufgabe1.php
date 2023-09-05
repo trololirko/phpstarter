85506879ff20b991a489b39d709e20e27a08d800
Trololirko <mirko.o.mueller@gmail.com>
2023-09-05 16:01:05 +0200

    Update

---
+++ aufgabe1.php
@@ -0,0 +1,62 @@
+<!-- // Schreibe eine Webbasierte Einkaufsliste
+// Über ein Formular werden Produkt-Namen eingetragen, die der Session hinzugefügt werden
+// Unterhalb des Formulars werde all in der Session enthaltenen Produkte angezeigt -->
+
+<?php
+
+include 'error_reporting.php';
+
+session_start();
+
+if(isset($_GET['reset'])) {
+    session_destroy();
+    header('Location: aufgabe1.php');
+}
+
+if(isset($_POST['produkt'])) {
+    $_SESSION['produkte'][] = $_POST['produkt'];
+}
+
+// if(isset($_SESSION['produkte'])) {
+//     print_r($_SESSION['produkte']);
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aufgabe 1</title>
+    <link rel="stylesheet" href="bootstrap5/css/bootstrap.min.css">
+
+</head>
+<body>
+    <main class="container mt-3">
+        <form action="aufgabe1.php" method="post">
+                <div class="mb-3">
+                    <label for="produkt" class="form-label">Produkt hinzufügen: </label>
+                    <input type="text" name="produkt" id="produkt" class="form-control">
+                </div>
+                
+                <div>
+                    <button type="submit" class="btn btn-secondary mb-1">Add</button>
+                </div>
+
+                <div>
+                    <a class="btn btn-warning mb-1" href="aufgabe1.php?reset" role="button">Reset</a>
+                </div>
+
+                <div class="mb-3">
+                    <h2>Einkaufsliste:</h2>
+                    <ul>
+                    <?php foreach($_SESSION['produkte'] as $produkt) { ?>
+                            <li><?php echo $produkt; } ?></li>
+                    </ul>
+                </div>
+        </form>
+    </main>
+</body>
+</html>
+
--- session2.php
+++ session2.php
@@ -1,15 +1,29 @@
 <?php
 
+include 'error_reporting.php';
+
 session_start();
 
 // isset prüft, ob eine Variable oder ein Array-Key vorhanden ist
-if(isset($_POST['user'])) {
-    $_SESSION['user'] = $_POST['user']; // Überträgt den Namen des Users aus dem POST in die Session
+if(isset($_POST['vorname'])) {
+    $_SESSION['user'] = ['vorname' => $_POST['vorname'], 'nachname' => $_POST['nachname']]; // Überträgt den Namen des Users aus dem POST in die Session
 }
 
 if(isset($_SESSION['user'])) {
-    echo 'Hallo '.$_SESSION['user'];
+
+    print_r($_SESSION);
+    // echo 'Hallo '.$_SESSION['user'];
+    echo 'Hallo '.$_SESSION['user']['vorname'].' '.$_SESSION['user']['nachname'];
+    echo '<p><a href="session2.php?logout=1">Logout</a></p>';
+    
 }
+
+if(isset($_GET['logout'])) {
+    session_destroy(); // Löscht die Session
+    // unset($_SESSION['user']); // Entfernt das Array am Key 'user'
+    header('Location: session2.php');
+}
+
 else {
     ?>
 
@@ -19,14 +33,40 @@ else {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link rel="stylesheet" href="bootstrap5/css/bootstrap.min.css">
     </head>
     <body>
-        
-        <form action="session2.php" method="post">
-            <label for="user">Name: </label>
-            <input type="text" name="user" id="user">
-            <button>Senden</button>
-        </form>
+
+        <main class="container">
+
+            <div class="row">
+                <div class="col-6">
+
+                    <h1>Deine Daten</h1>
+
+                    <form action="session2.php" method="post">
+
+                        <div class="mb-3">
+                            <label for="vorname" class="form-label">Vorname: </label>
+                            <input type="text" name="vorname" id="vorname" class="form-control">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="nachname" class="form-label">Nachname: </label>
+                            <input type="text" name="nachname" id="nachname" class="form-control">
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-secondary">Senden</button>
+                            <button type="submit" class="btn btn-secondary">Logout</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </main>
+
+    <script src="bootstrap5/js/bootstrap.min.js"></script>
 
     </body>
     </html>
